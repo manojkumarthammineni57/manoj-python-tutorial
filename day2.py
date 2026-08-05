a57c323617b1e967f1122565a3d87065875e7d9a
manoj <manojkumarthammineni57@gmail.com>
2026-08-05 09:16:09 -0700

    add day2.py and hello.py

---
+++ day2.py
@@ -0,0 +1,35 @@
+'manoj'
+print("manoj")
+
+'manoj' 'reddy'
+print('manoj' 'reddy')
+
+'manoj reddy'
+print('manoj reddy')
+
+name='manoj'
+name+='manoj'
+print(name)
+
+text='manoj'
+print('text')
+text[0]
+print(text[0])
+print(text[1])
+print(text[4])
+print(text[-1])
+print(text[0:3])
+print(text[-3:3])
+print(text[1:])
+print(text[:5])
+
+name='text'
+print(len('text'))
+
+text='my name is manoj reddy, \n welcome to college'
+print('text')
+
+text=''''''
+  
+
+
