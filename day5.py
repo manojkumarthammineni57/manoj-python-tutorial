b1b0abe296310f244864e2f3f2db2f4ce7caf69b
manoj <manojkumarthammineni57@gmail.com>
2026-08-08 09:46:16 -0700

    day 5 python sets

---
+++ day5.py
@@ -0,0 +1,24 @@
+set={1,2,3,4}
+print(set)
+
+set1={'a,b,c,d,n,k,t,p'}
+set2={'a,e,i,o,u,p,d'}
+print(set1&set2)
+
+set1={'manoj',9.8,987}
+set2={'naveen',4.5,54}
+print(set1)
+print(set2)
+
+1 in set
+print(1 in set)
+7 in set
+print(7 in set)
+len(set)
+print(len(set))
+type(set)
+print(type(set))
+
+set5=(6)
+set6=(7)
+print(set5&set6)      
