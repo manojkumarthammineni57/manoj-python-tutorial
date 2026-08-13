a8539c6c2c6b62eedd9333682934f2629c6055fd
manoj <manojkumarthammineni57@gmail.com>
2026-08-13 08:57:49 -0700

    day 9 python practice

---
+++ day9.py
@@ -0,0 +1,33 @@
+a=3
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+a+=2
+print(a+2)
+a-=2
+print(a-2)
+a*=8
+print(a*8)
+x,y=5,6
+print(x,y)
+a=6
+b=3
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+a=10
+b=15
+print(a<11 and b>5)
+print(a<11 and b<15)
+print(a<11 or b<6)
+print(a<15 or b<7)
+result='true'
+print(result)
+print(not result)
