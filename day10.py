bba0072b576b3c8ef7d052fbc4f481c6d31f68b1
manoj <manojkumarthammineni57@gmail.com>
2026-08-14 09:07:42 -0700

    day 10 practice python

---
+++ day10.py
@@ -0,0 +1,16 @@
+def add():
+    a=6
+    b=7
+    c=a+b
+    print(c)
+add()
+def add(x,y):
+    a=x
+    b=y
+    c=x+y
+    return c
+add(6,7)
+add(7,8)
+add(10,11)
+result=add(6,7)
+print("result is", result)
