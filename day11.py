215cfa456a551d0f5c6848424c664093b4b71823
manoj <manojkumarthammineni57@gmail.com>
2026-08-20 09:02:56 -0700

    day 11 python practice

---
+++ day11.py
@@ -0,0 +1,32 @@
+import math
+num=25
+result=math.sqrt(num)
+print(result)
+
+import math as m
+num=50
+result=m.sqrt(num)
+print(result)
+
+from math import sqrt
+num=60
+result=m.sqrt(num)
+print(result)
+
+from math import sqrt
+num=25
+result=sqrt(num)
+result1=math.floor(5.9)
+result2=math.ceil(5.9)
+result3=math.pow(5,3)
+print(result)
+print(result1)
+print(result2)
+print(result3)
+
+import math
+x=-10.7
+y=3
+print(math.ceil(x))
+print(math.fabs(y))
+print(pow(y,2))
