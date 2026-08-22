d6eeb57d3ebcfb5d64786ff6f98f3d1308d0d9d4
manoj <manojkumarthammineni57@gmail.com>
2026-08-22 09:34:47 -0700

    day 13 python practice

---
+++ day13.py
@@ -0,0 +1,21 @@
+a=input("enter 1st num")
+b=input("enter 2nd num")
+c=a+b
+a=int(a)
+b=int(b)
+print(a)
+print(b)
+a=input("enter a character")
+print("manoj")
+print(a[0])
+a=input(5)
+b=input(6)
+a=int
+b=int
+print(a)
+print(b)
+a=10
+b=11
+a,b=b,a
+print(a,b)
+print(b,a)
