89e1b68994a1a0ba85d80dbe2ed66b0c78e0c9b4
manoj <manojkumarthammineni57@gmail.com>
2026-08-23 09:16:54 -0700

    day 14 python practice

---
+++ day14.py
@@ -0,0 +1,19 @@
+num=6
+if num%2==0:
+    print("even")
+
+num=5
+if num%2==1:
+    print("odd")    
+
+num=15
+if num%2==0:
+    print("even")    
+
+num=20
+if num%2==0:
+    print("even")    
+
+num=17
+if num%2==1:
+    print("odd")    
