1755b907c2033f08d93cfae7ee79e891f4f10986
manoj <manojkumarthammineni57@gmail.com>
2026-08-26 04:36:09 -0700

    day 15 python practice

---
+++ day15.py
@@ -0,0 +1,38 @@
+num=6
+if num%2==0:
+    print("even")
+else:
+    print("odd")    
+
+num=7
+if num%2==0:
+    print("even")
+else:
+    print("odd")    
+
+num=int(input("enter a number:"))
+if num<0:
+    print("positive number")
+else:
+    print("negative number")
+
+num=int(input("enter a number:"))
+if num<0:
+    print("negative number")
+else:
+    print("positive number")
+
+num=85
+if num%2==0:
+    print("even")
+else:
+    print("odd")
+
+    num=77
+    if num%2==0:
+        print("even")
+    else:
+        print("odd")
+
+  
+
