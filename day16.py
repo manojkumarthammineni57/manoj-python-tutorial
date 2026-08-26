7b0e6c80d78ccb010a8c27b1b8ed7766b4544036
manoj <manojkumarthammineni57@gmail.com>
2026-08-26 05:38:34 -0700

    python practice day 16

---
+++ day16.py
@@ -0,0 +1,28 @@
+num=6
+salary=5
+if num%2==0:
+    print("even")
+    if salary>5:
+        print("great job")
+    else:
+        print("better luck next time")
+        num=7
+        salary=5
+        if num%2==0:
+            print("even")
+            if salary>5:
+                print("great job")
+            else:
+                print("bettrt luck next time")
+            num=100 
+            marks=90
+            if marks>50:
+                    print("pass")
+                    if marks>=90:
+                        print("distinction")
+                    else:
+                        print("avarage")
+            else:
+                    print("fail")
+
+            
