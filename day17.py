5eaa8d07bd5709a0836f81facd538163f428a4df
manoj <manojkumarthammineni57@gmail.com>
2026-08-31 05:01:50 -0700

    python practice

---
+++ day17.py
@@ -0,0 +1,33 @@
+num=1
+print("one")
+print("two")
+print("three")
+print("four")
+print("five")
+
+num=5
+if num==1:
+    print("one")
+    if num==2:
+        print("two")
+        if num==3:
+            print("three")
+            if num==4:
+                print("four")
+                if num==5:
+                    print("five")
+
+                    num=10
+                    if num==1:
+                        print("one")
+                    elif num==2:
+                        print("two")
+                    elif num==3:
+                        print("three")
+                    elif num==4:
+                        print("four")
+                    elif num==5:
+                        print("five")
+                    else:
+                        print("incorrect")    
+                    
