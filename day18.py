ec515c3559997b84335a51a6b8d7c19bdf505ec3
manoj <manojkumarthammineni57@gmail.com>
2026-09-01 05:30:55 -0700

    day18 python practice

---
+++ day18.py
@@ -0,0 +1,23 @@
+i=1
+while i<=5:
+    print("manoj")
+    i+=1
+print(i)
+
+i=1
+while i<5:
+    print("kumar")
+    i+=1
+
+    i=1 
+    while i<5:
+        print("manoj",end="")
+        j=1
+        while j<=4:
+            print("rocks",end="")
+            j+=1
+            i+=1
+            print()
+            print(i)
+
+    
