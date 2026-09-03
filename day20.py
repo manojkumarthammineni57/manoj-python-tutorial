941b271cc3e77a5de3e34fad0cc499c7b9869ba2
manoj <manojkumarthammineni57@gmail.com>
2026-09-03 06:01:19 -0700

    day20 python practice

---
+++ day20.py
@@ -0,0 +1,19 @@
+for value in range(10):
+    if value % 3 == 0:
+        print(value)
+        continue
+
+for value in range(10):
+    if value ==0:
+            print(value)
+            break
+
+for value in range(30):
+    if value %5==0:
+                print(value)
+                continue
+
+for value in range(50):
+    if value ==0:
+                    print(value)
+                    break 
