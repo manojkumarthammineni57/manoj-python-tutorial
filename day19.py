1aa12e2942f2944a97918b1e576b5b8113b4b75f
manoj <manojkumarthammineni57@gmail.com>
2026-09-03 05:15:30 -0700

    day18 python practice

---
+++ day19.py
@@ -0,0 +1,15 @@
+data=[2,'manoj',4.5,66,'jagan','vihan']
+for value in data:
+    print(value)
+
+d=['car','cycle','bike','motor']
+for value in d:
+    print(value)    
+
+d=['manoj','vihan','jagan','venu','gopal']
+for value in range(10):
+    print(value) 
+
+d=[1,2,3,4,5,6,7,8,9]
+for value in range(20):
+    print(value)        
